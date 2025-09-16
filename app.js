a456c070938b669a5646ae194395903bafcceadf
Laura J Duque P <duquelaura295@gmail.com>
2025-09-16 18:26:12 -0400

    El usuario puede agregar los nombres mediante el boton de añadir. Se limpia el espacio cada vez que agrega un nombre. Podrá ver en consola la lista de los nombres agregados.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let users = [];
+
+// funcion para agregar los nombres de los amigos
+
+function executeValor() {
+  return document.getElementById("amigo").value;
+}
+
+function addFriends(name) {
+  let valorname = executeValor();
+  if (valorname.trim() !== "") {
+    //  Guardamos en el array
+    users.push(valorname);
+
+    // Mostramos en consola
+    console.log(users);
+
+    //  Limpiamos el input
+    document.getElementById("amigo").value = "";
+  } else {
+    console.log("El campo está vacío");
+  }
+}
